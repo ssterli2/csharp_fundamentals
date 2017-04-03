b28686e14dbf2223fdc303bdddb54c8d0dbf36ad
Stephanie <ssterli2@gmail.com>
2017-04-03 12:00:04 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+            for (int i = 1; i < 100; i+=2)
+            {   
+                if (i%3 == 0 && i%5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i%3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i%5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            }
+             for (float i = 1; i < 100; i+=2)
+            {   
+                if (i/3 - Math.Floor(i/3) == 0 && i/5 - Math.Floor(i/5) == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i/3 - Math.Floor(i/3) == 0) 
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i/5 - Math.Floor(i/5) == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            }
+            Random rand = new Random();
+            for (int i = 1; i <= 10; i++)
+            {   
+                int randomNum = rand.Next();
+                Console.WriteLine(randomNum);
+                if (randomNum%3 == 0 && randomNum%5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (randomNum%3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (randomNum%5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            }
+        }
+    }
+}
